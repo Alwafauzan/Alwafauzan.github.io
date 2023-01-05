0ef386f8c42099a8295907112f07f87b61ff4eeb
Alwafauzan <88270178+Alwafauzan@users.noreply.github.com>
2023-01-05 12:40:56 +0700

    app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+# from crypt import methods
+from operator import mod
+from flask import Flask, request
+from flask import Flask, render_template
+import joblib
+# import sklearn
+# from sklearn.neighbors import KNeighborsClassifier
+import numpy as np
+app = Flask(__name__)
+
+@app.route("/" , methods=['POST','get'])
+def index():
+    return render_template('flask.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
